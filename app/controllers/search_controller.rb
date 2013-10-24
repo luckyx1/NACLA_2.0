74f0b89848a547b5257cdb9995fca6cd721c5bf5
Charlie Truong <tcharlie@server.fake>
2013-10-24 02:55:57 -0700

    Add tables, list-all button, basic search functionality.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,13 @@ class SearchController < ApplicationController
 	def index
 		@count = Document.count
 		@search_categories = Document.search_categories
-		@documents = Document.where(:title => "sdasdasfgsed")
+		@documents = nil
+
+		if params[:commit] == "List All"
+			@documents = Document.all
+		elsif params[:q] and params[:category]
+			@documents = Document.where(params[:category] => params[:q])
+		end
+
 	end
 end
