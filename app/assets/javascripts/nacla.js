980261cc79f2f25478a190765f58068d37bcac39
Andrew Dorsett <awdorsett@berkeley.edu>
2013-11-01 13:51:28 -0700

    switched to AngularJS and updated page

---
+++ app/assets/javascripts/nacla.js
@@ -0,0 +1,28 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: saasbook
+ * Date: 11/1/13
+ * Time: 11:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+app = angular.module("NACLA", ["ngResource"])
+
+
+function CreateCoursePackCtrl($scope,$resource){
+
+    $scope.all_articles = $resource('all_articles').query() ;
+    $scope.selected_articles = [];
+    $scope.search_input = '';
+
+    $scope.add_to_selected = function(index){
+        if($scope.selected_articles.indexOf($scope.all_articles[index]) == -1)
+            $scope.selected_articles.push($scope.all_articles[index]);
+    };
+
+    $scope.remove_selected = function(index){
+        $scope.selected_articles.splice(index,1);
+    };
+
+
+}
