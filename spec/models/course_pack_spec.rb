24c45a723294dab8b09d023bde55578021a291c0
saasbook <saasbook@saasbook.(none)>
2013-11-10 16:36:44 -0800

    model rspec done

---
+++ spec/models/course_pack_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe CoursePack do
+	describe 'belongs to Article' do
+		before do
+			@pack = CoursePack.create!
+		end
+		it "should have Article method" do
+			@pack.should respond_to (:articles)
+		end
+
+	end
+end
