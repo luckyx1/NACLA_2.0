c235d81a5f6ecd408523f22d73b8cf26190eff00
Charlie Truong <tcharlie@server.fake>
2013-11-10 20:32:50 -0800

    Add small TDD for search

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe SearchController do
 
-	describe "search index" do
+	describe "visit search index" do
 		it "returns http success" do
 			get 'index'
 			response.should be_success
