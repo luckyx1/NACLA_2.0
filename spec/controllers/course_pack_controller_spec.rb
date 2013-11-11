729a5e301845283fde4cb856dc0473985cc9cab1
saasbook <saasbook@saasbook.(none)>
2013-11-10 21:12:44 -0800

    working on TDD

---
+++ spec/controllers/course_pack_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe CoursePacksController do
+
+  describe "Create method" do
+    it "should redirect to '/' on an html request"
+    end
+  end
+
+  describe "try to create and save a new course pack from json request" do 	
+  	it "should return a 'conflict http status if fails" do
+  	end
+
+  	it "should respond with an 'ok' request if success" do
+  	end
+  end
+
+
+
+
