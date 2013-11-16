0e6cad578d6fc2f00cde4ab66487f06732f36147
Andrew Dorsett <awdorsett@berkeley.edu>
2013-11-16 14:34:23 -0800

    idea

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131116212854) do
+ActiveRecord::Schema.define(:version => 20131116222658) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(:version => 20131116212854) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
+    t.boolean  "public"
+    t.text     "tags"
   end
 
   create_table "documents", :force => true do |t|
