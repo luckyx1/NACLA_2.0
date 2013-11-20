fc47b9c916351f2b8320983f5f0d6d84238f86b5
Andrew Dorsett <awdorsett@berkeley.edu>
2013-11-19 21:12:00 -0800

    removed migration

--- db/migrate/20131116224545_remove_download_link_from_articles.rb
+++
@@ -1,9 +0,0 @@
-class RemoveDownloadLinkFromArticles < ActiveRecord::Migration
-  def up
-    remove_column :articles, :download_link
-  end
-
-  def down
-    add_column :articles, :download_link, :string
-  end
-end
