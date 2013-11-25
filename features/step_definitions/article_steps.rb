8c6bd5857fb2d7129b360f62e4fb7c335f0b79fe
saasbook <saasbook@saasbook.(none)>
2013-11-24 17:11:16 -0800

    reovered test

--- features/step_definitions/article_steps.rb
+++ features/step_definitions/article_steps.rb
@@ -2,8 +2,8 @@ Given(/^I am on the article search page$/) do
 	visit "/search"
 end
 
-Given(/^I click "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^I click "(.*?)"$/) do |button|
+	click_on button
 end
 
 When(/^I have not entered a search query$/) do
--- features/step_definitions/create_coursepack_steps.rb
+++ features/step_definitions/create_coursepack_steps.rb
@@ -11,7 +11,6 @@ Given(/^I am on the new coursepack page$/) do
 end
 
 When (/^I fill in '([^"]*)' with '([^"]*)'$/) do |field, value|
-  put page
   fill_in  field.downcase.to_str, :with => value
 end
 
