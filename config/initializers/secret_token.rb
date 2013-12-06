1facb986f9d735edc820f4ae772a2bd1057c9bc7
saasbook <saasbook@saasbook.(none)>
2013-12-06 12:37:18 -0800

    securing our previously public secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-NACLA::Application.config.secret_token = '9c5a51582fc9cec050c9e63df362bc9a68977f7b2d2f358ff6398a07083ea6fd7d4616e44a1d7e59d3fe4eb464aab4d7aebd6d584598781d0674db69c52e39e8'
+NACLA::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  '9c5a51582fc9cec050c9e63df362bc9a68977f7b2d2f358ff6398a07083ea6fd7d4616e44a1d7e59d3fe4eb464aab4d7aebd6d584598781d0674db69c52e39e8'
+else
+  ENV['SECRET_TOKEN']
+end
+
