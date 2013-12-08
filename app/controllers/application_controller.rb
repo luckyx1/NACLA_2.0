7825c2d30c4becb76f39eebb36faa0caf7977ce4
Andrew Dorsett <awdorsett@berkeley.edu>
2013-12-07 19:18:37 -0800

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
-  before_filter :back_url
+  before_filter :back_url, :featured_course_packs
 
   private
   def current_user
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
 
   def back_url
     @back_url = request.referer
+  end
+
+  def featured_course_packs
+    @featured_course_packs = CoursePack.where(:featured=>true).limit(5)
   end
   
   protected 
