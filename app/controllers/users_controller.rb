248483450677e1020a5db709837629e1efd93ab7
saasbook <saasbook@saasbook.(none)>
2013-12-08 01:13:41 -0800

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,6 +54,9 @@ class UsersController < ApplicationController
       coursepack.summary = coursepack.summary[0...300] + '...' if coursepack.summary.length > 300
     end
     @comments = Comment.find_all_by_user_id(@user.id, :order => "created_at desc", :limit => 10) || []
+    @comments.each do |comment|
+      comment.comment = comment.comment[0...140] + '...' if comment.comment.length > 140
+    end
   end
   
   def index
