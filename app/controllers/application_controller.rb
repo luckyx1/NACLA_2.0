ebb492c9782c6d518c2ea99c16e5636370a6b871
Andrew Dorsett <awdorsett@berkeley.edu>
2013-12-07 18:42:23 -0800

    crap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
+  before_filter :back_url
+
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def back_url
+    @back_url = respond.referrer
   end
   
   protected 
